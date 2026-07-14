f0bd32392dcd828db7158a0ca14af0631528f862
flowey-pc <stepanmasirov@gmail.com>
2026-07-14 22:43:44 +0500

    Optimize keybinding

--- src/main/java/fiskfille/tf/client/keybinds/TFKeyBinds.java
+++ src/main/java/fiskfille/tf/client/keybinds/TFKeyBinds.java
@@ -5,12 +5,12 @@ import org.lwjgl.input.Keyboard;
 import cpw.mods.fml.client.registry.ClientRegistry;
 
 public class TFKeyBinds {
-	public static TFKeyBinding keyBindingTransform1 = new TFKeyBinding("key.transform1", Keyboard.KEY_C);
-	public static TFKeyBinding keyBindingStealthMode = new TFKeyBinding("key.stealth_force", Keyboard.KEY_V);
-	public static TFKeyBinding keyBindingZoom = new TFKeyBinding("key.aim", Keyboard.KEY_B);
-	public static TFKeyBinding keyBindingBrake = new TFKeyBinding("key.brake", Keyboard.KEY_Z);
-	public static TFKeyBinding keyBindingVehicleFirstPerson = new TFKeyBinding("key.first_person", Keyboard.KEY_G);
-	public static TFKeyBinding keyBindingViewFront = new TFKeyBinding("key.rear_view", Keyboard.KEY_R);
+	public static final TFKeyBinding keyBindingTransform1 = new TFKeyBinding("key.transform1", Keyboard.KEY_C);
+	public static final TFKeyBinding keyBindingStealthMode = new TFKeyBinding("key.stealth_force", Keyboard.KEY_V);
+	public static final TFKeyBinding keyBindingZoom = new TFKeyBinding("key.aim", Keyboard.KEY_B);
+	public static final TFKeyBinding keyBindingBrake = new TFKeyBinding("key.brake", Keyboard.KEY_Z);
+	public static final TFKeyBinding keyBindingVehicleFirstPerson = new TFKeyBinding("key.first_person", Keyboard.KEY_G);
+	public static final TFKeyBinding keyBindingViewFront = new TFKeyBinding("key.rear_view", Keyboard.KEY_R);
 
 	public static void register() {
 		ClientRegistry.registerKeyBinding(keyBindingTransform1);
