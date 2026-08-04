10166b2069d9896fb7899e90ea4b37dd557d2748
flowey-pc <stepanmasirov@gmail.com>
2026-08-04 17:22:03 +0500

    Fix crash on server

--- src/main/java/fiskfille/tf/common/network/MessagePlayerData.java
+++ src/main/java/fiskfille/tf/common/network/MessagePlayerData.java
@@ -4,6 +4,8 @@ import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import fiskfille.tf.TransformersMod;
 import fiskfille.tf.client.audio.MovingSoundTransformer;
 import fiskfille.tf.common.data.TFData;
@@ -54,36 +56,47 @@ public class MessagePlayerData implements IMessage {
 	public static class Handler implements IMessageHandler<MessagePlayerData, IMessage> {
 		@Override
 		public IMessage onMessage(MessagePlayerData message, MessageContext ctx) {
-			TFData type = message.type;
-			Object value = message.value;
-
 			if(ctx.side.isClient()) {
-				final Entity entity = TransformersMod.proxy.getPlayer().worldObj.getEntityByID(message.id);
+				return client(message, ctx);
+			}
 
-				if(entity instanceof EntityPlayer) {
-					type.setWithoutNotify((EntityPlayer) entity, value);
+			return server(message, ctx);
+		}
 
-					if(type == TFData.ALT_MODE) {
-						final Transformer transformer = TFHelper.getTransformer((EntityPlayer) entity);
+		@SideOnly(Side.CLIENT)
+		private IMessage client(MessagePlayerData message, MessageContext ctx) {
+			final Entity entity = TransformersMod.proxy.getPlayer().worldObj.getEntityByID(message.id);
 
-						if(transformer != null) {
-							Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundTransformer(entity, transformer.getTransformationSound((Integer) value)));
-						}
-					}
-					else if(type == TFData.STEALTH_FORCE) {
-						Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundTransformer(entity, new ResourceLocation(TransformersMod.MODID, "transform_stealth" + ((Boolean) value ? "" : "_in"))));
+			if(entity instanceof EntityPlayer) {
+				final TFData type = message.type;
+				final Object value = message.value;
+
+				type.setWithoutNotify((EntityPlayer) entity, value);
+
+				if(type == TFData.ALT_MODE) {
+					final Transformer transformer = TFHelper.getTransformer((EntityPlayer) entity);
+
+					if(transformer != null) {
+						Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundTransformer(entity, transformer.getTransformationSound((Integer) value)));
 					}
 				}
+				else if(type == TFData.STEALTH_FORCE) {
+					Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundTransformer(entity, new ResourceLocation(TransformersMod.MODID, "transform_stealth" + ((Boolean) value ? "" : "_in"))));
+				}
 			}
-			else {
-				final EntityPlayer player = ctx.getServerHandler().playerEntity;
 
-				if(player != null) {
-					final Entity entity = player.worldObj.getEntityByID(message.id);
+			return null;
+		}
 
-					if(entity instanceof EntityPlayer) {
-						type.set((EntityPlayer) entity, value);
-					}
+		@SideOnly(Side.SERVER)
+		private IMessage server(MessagePlayerData message, MessageContext ctx) {
+			final EntityPlayer player = ctx.getServerHandler().playerEntity;
+
+			if(player != null) {
+				final Entity entity = player.worldObj.getEntityByID(message.id);
+
+				if(entity instanceof EntityPlayer) {
+					((TFData) message.type).set((EntityPlayer) entity, message.value);
 				}
 			}
 
